127c57d0a7bdad3b1761ddd84bce5cecb66d3b40
Denzale Houston <denzalethouston@gmail.com>
2020-11-11 20:10:26 -0700

    changed name

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Snakeeatter1998",
+    password: "Snakeeater1998",
     database: "burger_db"
 });
 
